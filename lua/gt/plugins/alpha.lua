4b579105c47ef8d0112392974e4e6ef4b3a737a5
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2024-04-10 23:00:30 -0400

    Remove auto-session for now

--- lua/gt/plugins/alpha.lua
+++ lua/gt/plugins/alpha.lua
@@ -23,7 +23,6 @@ return {
 			dashboard.button("<leader>ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
 			dashboard.button("<leader>ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("<leader>fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("<leader>wr", "󰁯  > Workspace Restore", "<cmd>SessionRestore<CR>"),
 			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
