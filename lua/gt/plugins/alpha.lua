51679e5f5e5545686b3c821a5671f815965ba729
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2024-04-12 23:13:13 -0400

    Update conf using kickstart content

--- lua/gt/plugins/alpha.lua
+++ lua/gt/plugins/alpha.lua
@@ -21,8 +21,8 @@ return {
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
 			dashboard.button("<leader>ee", "  > Toggle file explorer", "<cmd>Neotree toggle<CR>"),
-			dashboard.button("<leader>ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("<leader>fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("<leader>sf", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("<leader>sw", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
 			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
--- lua/gt/plugins/treesitter.lua
+++ lua/gt/plugins/treesitter.lua
@@ -1,65 +1,68 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	opts = {
+		ensure_installed = {
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"prisma",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"c",
+			"java",
+			"kotlin",
+			"groovy",
+			"matlab",
+			"go",
+			"sql",
+			"rust",
+			"zig",
+			"dockerfile",
+		},
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-        "java",
-        "kotlin",
-        "groovy",
-        "matlab",
-        "go",
-        "sql",
-        "rust",
-        "zig",
-        "dockerfile",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { "ruby" },
+		},
+		indent = { enable = true, disable = { "ruby" } },
+	},
+	config = function(_, opts)
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup(opts)
+
+		-- There are additional nvim-treesitter modules that you can use to interact
+		-- with nvim-treesitter. You should go explore a few and see what interests you:
+		--
+		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+	end,
 }
