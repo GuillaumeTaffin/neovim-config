53337a06df0992b43c6f587384d99b273d6b2769
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2024-04-19 20:40:11 -0400

    :q

--- lua/gt/plugins/neo-tree.lua
+++ lua/gt/plugins/neo-tree.lua
@@ -9,17 +9,34 @@ return {
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
+        --  INFO: Keymaps
         require("which-key").register({
             ["<leader>we"] = { name = "[E]xplorer", _ = "which_key_ignore" },
         })
         vim.keymap.set("n", "<leader>wee", "<cmd>Neotree toggle<CR>", { desc = "[E]xpand toggle" })
         vim.keymap.set("n", "<leader>wer", "<cmd>Neotree filesystem reveal<CR>", { desc = "[R]eveal current file" })
-        vim.keymap.set("n", "<leader>K", "<cmd>Neotree float git_status git_base=HEAD<CR>", { desc = "[K]ommit" })
         vim.keymap.set("n", "<leader>K", function()
             local commands = require("neo-tree.sources.common.commands")
             vim.cmd("Neotree git_status git_base=HEAD")
+            print(vim.api.nvim_get_current_buf())
             commands.git_add_all()
             commands.git_commit()
         end, { desc = "[K]ommit" })
+
+        --  INFO: Icons
+        vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+        vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+        vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+        vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+        -- INFO: Config
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
+            },
+        })
     end,
 }
